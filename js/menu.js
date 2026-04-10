2da41155fd21907f5dc140a703278020a62c2b81
Livia2710 <130663916+Livia2710@users.noreply.github.com>
2026-04-10 14:56:53 -0300

    Padronizando as classes

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,37 @@
-document.getElementById("cardLancar").addEventListener("click", function() {
+const cardLancar = document.getElementById("cardLancar");
+const modal = document.getElementById("modalEscolha");
+
+const irPagina = document.getElementById("irPagina");
+const abrirModal = document.getElementById("abrirModal");
+const fecharModal = document.getElementById("fecharModal");
+
+// abre modal de escolha
+cardLancar.addEventListener("click", () => {
+    modal.classList.add("show");
+});
+
+// ir para página normal
+irPagina.addEventListener("click", () => {
     window.location.href = "TelaRegistro.html";
+});
+
+// abrir como modal (vamos fazer agora)
+abrirModal.addEventListener("click", () => {
+    modal.classList.remove("show");
+    document.getElementById("modalRegistro").classList.add("show");
+});
+
+// fechar escolha
+fecharModal.addEventListener("click", () => {
+    modal.classList.remove("show");
 });
 
 document.getElementById("cardConsultar").addEventListener("click", function() {
     window.location.href = "ConsultarHoras.html";
+});
+
+const fecharRegistro = document.getElementById("fecharRegistro");
+
+fecharRegistro.addEventListener("click", () => {
+    document.getElementById("modalRegistro").classList.remove("show");
 });
